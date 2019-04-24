f1cb1908527181a79b133c1ea618e069dcf2c6f2
Kentaro91011 <kentaro.10.11@icloud.com>
2019-04-24 19:56:14 +0900

    file追記

---
+++ components/ShareButtons.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <no-ssr>
+      <div class="ShareButtons">
+        <div>
+          <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+        <div>
+          <!-- facebookシェアボタン -->
+          <div id="fb-root"></div>
+          <span ref="unique-parameter-value" class="facebook-share-button fb-share-button" data-size="small" data-layout="button" data-mobile-iframe="true">
+          </span>
+        </div>
+        <!-- LINEシェアボタン  -->
+        <!-- <div class="line-it-button" data-lang="ja" data-type="share-a" data-ver="3" data-url="https://4792f276.ngrok.io#lineitbutton" data-color="default" data-size="small" data-count="false" style="display: none;"></div>
+        <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script> -->
+      </div>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShareButtons',
+  mounted () {
+    ;(function (d, s, id) {
+      window.FB = null
+      var js
+      var fjs = d.getElementsByTagName(s)[0]
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.12'
+      fjs.parentNode.insertBefore(js, fjs)
+    })(document, 'script', 'facebook-jssdk')
+  }
+}
+</script>
+
+<style scoped>
+.ShareButtons {
+  display: flex;
+}
+
+.ShareButtons > div {
+  margin-right: 8px;
+}
+
+.fb-share-button {
+  line-height: 0 !important;
+}
+
+.fb_iframe_widget_fluid {
+    display: block;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,72 @@
+import Vuex from 'vuex'
+
+const store = () => new Vuex.Store({
+
+  actions: {
+    async nuxtServerInit ({ commit, state }) {
+      let meta = await this.$axios.get(state.wordpressAPI)
+      commit('setMeta', meta.data)
+    }
+  },
+
+  state: {
+    article: null,
+    articles: [],
+    page: null,
+    authorArticles: [],
+    authors: null,
+    indexInfiniteLoading: {
+      enabled: true,
+      page: 1
+    },
+    featuredArticles: [],
+    featuredColor: {},
+    // featuredID: 194,
+    featuredID: 4,
+    meta: {
+      description: '',
+      name: ''
+    },
+    topicArticles: [],
+    topics: null,
+    wordpressAPI: 'http://niko0420.sakura.ne.jp/wp-01/wp-json'
+  },
+
+  mutations: {
+    setArticle (state, data) {
+      state.article = data
+    },
+    setArticles (state, data) {
+      state.articles = state.articles.concat(data)
+    },
+    setPage (state, data) {
+      state.page = data
+    },
+    setAuthorArticles (state, data) {
+      state.authorArticles.push(data)
+    },
+    setAuthors (state, data) {
+      state.authors = data
+    },
+    setIndexInfiniteLoading (state, data) {
+      state.indexInfiniteLoading = data
+    },
+    setFeaturedColor (state, data) {
+      state.featuredColor = data
+    },
+    setFeaturedArticles (state, data) {
+      state.featuredArticles = state.featuredArticles.concat(data)
+    },
+    setMeta (state, data) {
+      state.meta = data
+    },
+    setTopicArticles (state, data) {
+      state.topicArticles.push(data)
+    },
+    setTopics (state, data) {
+      state.topics = data
+    }
+  }
+})
+
+export default store
