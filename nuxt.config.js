4e38321043616c527278c72b1a29df4dd39807ea
Kentaro91011 <kentaro.10.11@icloud.com>
2019-05-01 13:13:47 +0900

    静的ファイル作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,6 +56,29 @@ module.exports = {
     { src: '~plugins/vue-tabs-with-active-line.js', ssr: false },
     { src: '~plugins/vue-burger-menu.js', ssr: false }
   ],
+  generate: {
+    interval: 1000,
+    routes () {
+      return Promise.all([
+        axios.get(`${apiUrl}/wp-json/wp/v2/posts?per_page=100&page=1&_embed=1`),
+        axios.get(`${apiUrl}/wp-json/wp/v2/pages?per_page=100&page=1&_embed=1`)
+      ]).then((data) => {
+        const posts = data[0]
+        const pages = data[1]
+        return posts.data.map((post) => {
+          return {
+            route: '/post/' + post.slug,
+            payload: post
+          }
+        }).concat(pages.data.map((page) => {
+          return {
+            route: page.slug,
+            payload: page
+          }
+        }))
+      })
+    }
+  },
 
   vendor: ['lightgallery.js']
 }
